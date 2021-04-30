f05728d769d788e46f070788e304deeabfe07335
Thinh Nguyen <gthinh.nguyen@gmail.com>
2021-04-30 14:29:45 -0400

    created basic styles

---
+++ styles/colors.ts
@@ -0,0 +1,18 @@
+const colors = {
+  text: "#000",
+  background: "#fff",
+  primary: "#07c",
+  secondary: "#30c",
+  muted: "#f6f6f6",
+  modes: {
+    dark: {
+      text: "#F0F5FA",
+      background: "#222639",
+      primary: "#C2A3FF",
+      secondary: "#EE99FF",
+      highlight: "#5F527A",
+      muted: "rgba(0, 0, 0, 0.2)",
+    },
+  },
+};
+export default colors;
---
+++ styles/theme.ts
@@ -0,0 +1,42 @@
+import { Theme } from 'theme-ui';
+import colors from './colors';
+import shadows from './shadows';
+import typography from './typography';
+
+export const theme: Theme = {
+  space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
+  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 96],
+  fontWeights: {
+    body: 400,
+    heading: 700,
+    bold: 700,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+  colors,
+  shadows,
+  text: {
+    block: {
+      fontFamily: 'inherit',
+      lineHeight: 'body',
+    },
+  },
+  styles: {
+    typography,
+  },
+  radii: {
+    card: '0.5rem',
+  },
+  breakpoints: ['48em', '64em'],
+  buttons: {
+    secondary: {
+      boxShadow: 'card',
+      cursor: 'pointer',
+      '&:hover': {
+        backgroundColor: 'black',
+      },
+    },
+  },
+};
