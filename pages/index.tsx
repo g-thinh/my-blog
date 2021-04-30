fd89180bb33caec0ac7a540e0d8ec0029f092dfd
Thinh Nguyen <gthinh.nguyen@gmail.com>
2021-04-30 14:32:42 -0400

    created homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
+import { Container, Box, Text, Paragraph } from "theme-ui";
 import { GetServerSideProps } from "next";
 import absoluteUrl from "next-absolute-url";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   try {
     const { origin } = absoluteUrl(context.req);
-    const apiURL = `${origin}/api/`;
+    const apiURL = `${origin}/api`;
     const res = await fetch(apiURL);
     const data = await res.json();
 
@@ -15,13 +16,64 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   }
 };
 
-export default function Home({ data }: any) {
+interface Props {
+  data: any;
+}
+
+export default function Home({ data }: Props) {
   return (
-    <div>
-      <h1>
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
-      <p>{data.content.title}</p>
-    </div>
+    <Container>
+      <Text
+        as="h1"
+        color="text"
+        sx={{
+          fontSize: [3, 4, 5],
+          textAlign: "center",
+          fontWeight: "bold",
+        }}
+      >
+        {data.content.title}
+      </Text>
+      <Text
+        as="p"
+        color="text"
+        my={[1, 3]}
+        sx={{ textAlign: "center", fontSize: [3] }}
+      >
+        {data.content.subtitle}
+      </Text>
+      <Box p={2} sx={{ borderRadius: "card" }}>
+        <Paragraph as="p" variant="block" my={3}>
+          {data.content.description}
+        </Paragraph>
+        <Paragraph as="p" variant="block" my={3}>
+          {data.content.header_tags}
+        </Paragraph>
+        {data.content.tags.map((tag: any) => {
+          return (
+            <Text
+              as="span"
+              key={tag}
+              px={1}
+              sx={{
+                filter: "brightness(57%)",
+                userSelect: "none",
+                "&::before": {
+                  content: '"#"',
+                },
+
+                "&:hover": {
+                  cursor: "pointer",
+                  transition: "all 0.3s",
+                  color: "primary",
+                },
+              }}
+            >
+              {tag}
+            </Text>
+          );
+        })}
+      </Box>
+    </Container>
   );
 }
