d0aecdea5a7a2f039b379ed107ac259f4e57112e
Thinh Nguyen <gthinh.nguyen@gmail.com>
2021-04-30 14:30:16 -0400

    created basic components for header and layout

---
+++ components/Header.tsx
@@ -0,0 +1,70 @@
+/** @jsxImportSource theme-ui */
+import { Box, Flex, Text } from 'theme-ui';
+import { RouteLink } from '@components/RouteLink';
+import NavButtons from '@components/NavButtons';
+
+const Header: React.FC = () => (
+  <Box as="header" mx={[0, 1]} my={[0, 3]} px={[0, 3]}>
+    <Flex
+      sx={{
+        flexDirection: ['column', 'row'],
+        flex: '5',
+        alignContent: 'center',
+        height: '100%',
+      }}
+    >
+      <RouteLink href="/" index={1}>
+        <Text
+          as="p"
+          my={[2, 0]}
+          sx={{
+            userSelect: 'none',
+            fontSize: [3, 4],
+            textAlign: 'center',
+          }}
+        >
+          Gia Thinh Nguyen
+          <Text color="primary" sx={{ fontWeight: 'bold' }}>
+            .
+          </Text>
+        </Text>
+      </RouteLink>
+      <Flex
+        as="ul"
+        py={[2, 0]}
+        sx={{
+          alignItems: 'center',
+          justifyContent: 'center',
+          flex: ['auto', '4'],
+          width: '100%',
+          backgroundColor: ['muted', 'transparent'],
+        }}
+      >
+        <RouteLink href="/about" index={2}>
+          About
+        </RouteLink>
+        <RouteLink href="/blog" index={3}>
+          Blog
+        </RouteLink>
+        <RouteLink href="/code" index={4}>
+          Code
+        </RouteLink>
+        <RouteLink href="/food" index={5}>
+          Food
+        </RouteLink>
+      </Flex>
+      <Flex
+        sx={{
+          display: ['none', 'flex'],
+          flex: '1',
+          width: '100%',
+          justifyContent: 'flex-end',
+        }}
+      >
+        <NavButtons />
+      </Flex>
+    </Flex>
+  </Box>
+);
+
+export default Header;
---
+++ components/Layout.tsx
@@ -0,0 +1,63 @@
+/** @jsxImportSource theme-ui */
+import { Flex, Box, Text } from 'theme-ui';
+import Header from '@components/Header';
+import NavButtons from '@components/NavButtons';
+
+const Layout: React.FC = (props) => (
+  <Flex
+    sx={{
+      display: 'flex',
+      flexDirection: 'column',
+      minHeight: '100vh',
+    }}
+  >
+    <Header />
+
+    <Flex
+      as="main"
+      sx={{
+        marginTop: [2, 4],
+        width: '100%',
+        flex: '1 1 auto',
+      }}
+    >
+      <Box
+        sx={{
+          maxWidth: '48em',
+          mx: 'auto',
+          px: [1, 3],
+        }}
+      >
+        {props.children}
+      </Box>
+    </Flex>
+    <Box
+      as="footer"
+      sx={{
+        width: '100%',
+      }}
+    >
+      <Box py={[2, 3]} bg="muted">
+        <Box
+          sx={{
+            display: ['block', 'none'],
+          }}
+        >
+          <NavButtons />
+        </Box>
+        <Text
+          as="p"
+          sx={{
+            fontSize: 0,
+            filter: 'brightness(75%)',
+            textAlign: 'center',
+          }}
+        >
+          © 2021 All rights probably deserved.
+        </Text>
+      </Box>
+    </Box>
+  </Flex>
+);
+
+export default Layout;
