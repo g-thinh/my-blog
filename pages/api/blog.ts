5421ea6e943ee76bc3d74ccf9ee5bc9659ea9e31
Thinh Nguyen <gthinh.nguyen@gmail.com>
2021-05-02 07:45:38 -0400

    added blog post page with api call

---
+++ pages/api/blog.ts
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+const baseURL = "https://api.storyblok.com/v1/cdn/stories";
+
+export default async (_req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const randomId = Math.floor(Math.random() * 10000);
+    const API_KEY = process.env.sbKey;
+    const response = await fetch(
+      `${baseURL}?filter_query[component][in]=blogpost&token=${API_KEY}&cv=${randomId}`
+    );
+    const { stories } = await response.json();
+    res.status(200).send(stories);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
---
+++ pages/blog/index.tsx
@@ -0,0 +1,46 @@
+import { Container, Grid } from "theme-ui";
+import { GetServerSideProps } from "next";
+import absoluteUrl from "next-absolute-url";
+import { Heading, Subheading, BlogPostCard } from "@components/index";
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  try {
+    const { origin } = absoluteUrl(context.req);
+    const apiURL = `${origin}/api/blog`;
+    const res = await fetch(apiURL);
+    const data = await res.json();
+    return { props: { data } };
+  } catch (error) {
+    console.log("Error", error);
+    return {
+      notFound: true,
+      redirect: {
+        destination: "/404",
+        permanent: false,
+      },
+      props: {},
+    };
+  }
+};
+
+export default function BlogPage({ data }: any) {
+  return (
+    <Container
+      my="auto"
+      sx={{
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <Heading>Blog Posts</Heading>
+      <Subheading>
+        Some of the discoveries I've made working as a front-end dev lately.
+      </Subheading>
+      <Grid sx={{ gridTemplateColumns: ["1fr"], gridAutoRows: "1fr" }}>
+        {data.map((post) => {
+          return <BlogPostCard key={post.id} data={post} />;
+        })}
+      </Grid>
+    </Container>
+  );
+}
