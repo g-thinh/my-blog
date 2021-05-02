e2375b244a27cd1433921935db116d853fc67b23
Thinh Nguyen <gthinh.nguyen@gmail.com>
2021-05-02 13:31:26 -0400

    added isCenter bool to heading

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -1,13 +1,13 @@
 import { Text } from "theme-ui";
 
-export const Heading = ({ children }) => (
+export const Heading = ({ children, isCenter = true }) => (
   <Text
     as="h1"
     color="text"
     my={[2, 3]}
     sx={{
       fontSize: [4, 5],
-      textAlign: "center",
+      textAlign: isCenter ? "center" : "left",
       fontWeight: "bold",
     }}
   >
