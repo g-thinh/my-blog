b290cfa943248c342d361e8afeead713c5ebca98
Thinh Nguyen <gthinh.nguyen@gmail.com>
2021-05-05 11:48:09 -0400

    cleared some ts errors in next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+// eslint-disable-next-line no-undef
 module.exports = {
   env: {
+    // eslint-disable-next-line no-undef
     sbKey: process.env.NEXT_PUBLIC_API_TOKEN,
+    // eslint-disable-next-line no-undef
     sbPreviewKey: process.env.NEXT_PUBLIC_API_PREVIEW,
   },
   images: {
