377209a95cb7b04b5f8acb0cf4f5ad144fdd639f
Thinh Nguyen <gthinh.nguyen@gmail.com>
2021-05-05 11:59:46 -0400

    code blog pages are now statically generated and preview mode enabled for storyblok

---
+++ pages/code/[slug].tsx
@@ -0,0 +1,90 @@
+import { Container, Text, Box, Badge, Divider } from "theme-ui";
+import { GetStaticPaths, GetStaticProps } from "next";
+import Storyblok, { useStoryblok } from "@utils/storyblok";
+import { Story } from "storyblok-js-client";
+import { format } from "date-fns";
+import { calculateReadTime } from "@utils/calculateReadTime";
+import { Heading, Subheading } from "@components/index";
+import { render } from "storyblok-rich-text-react-renderer";
+import { resolvers } from "@utils/StoryblokResolvers";
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  try {
+    const { slug } = context.params;
+    const params: Params = {
+      version: "published",
+    };
+
+    if (context.preview) {
+      params.version = "draft";
+      params.cv = Date.now();
+    }
+
+    const { data } = await Storyblok.get(`cdn/stories/code/${slug}`, params);
+
+    return {
+      props: {
+        story: data ? data.story : false,
+        preview: context.preview || false,
+      },
+      revalidate: 10,
+    };
+  } catch (error) {}
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await Storyblok.get("cdn/links/", {
+    starts_with: "code",
+  });
+
+  const paths = [];
+  Object.keys(data.links).forEach((linkKey) => {
+    if (data.links[linkKey].is_folder || data.links[linkKey].slug !== "code") {
+      return;
+    }
+    const slug = data.links[linkKey].slug.replace("code", "");
+    paths.push({ params: { slug } });
+  });
+
+  return {
+    paths: paths,
+    fallback: "blocking",
+  };
+};
+
+interface Props {
+  story: Story;
+}
+
+export default function CodePostPage(props: Props): JSX.Element {
+  const story = useStoryblok(props.story);
+  return (
+    <Container p={[2, 3]} sx={{ width: "100%" }}>
+      <h1>Hello</h1>
+      <Heading isCenter={false}>{story.name}</Heading>
+
+      <Text as="h2" color="grey" sx={{ textAlign: "left" }}>
+        {format(new Date(story.first_published_at), "MMM d")} •{"  "}
+        {calculateReadTime(story.content.long_text.content)}
+      </Text>
+      {story.content.tags &&
+        story.content.tags.map((tag) => (
+          <Badge
+            key={tag}
+            mr={3}
+            px={2}
+            sx={{
+              backgroundColor: "primary",
+              color: "text",
+              borderRadius: "1rem",
+            }}
+          >
+            {tag}
+          </Badge>
+        ))}
+      <Divider />
+      <Subheading>{story.content.title}</Subheading>
+      <Box pb={[3, 4]}>{render(story.content.long_text, resolvers)}</Box>
+    </Container>
+  );
+}
--- pages/code/index.tsx
+++ pages/code/index.tsx
@@ -1,30 +1,42 @@
 import { Container, Grid, Text, Card, Divider, Flex } from "theme-ui";
 import { Heading, Subheading } from "@components/index";
-import { GetServerSideProps } from "next";
+import Storyblok, { useStoryblok } from "@utils/storyblok";
+import { GetStaticProps } from "next";
 import Link from "next/link";
-import absoluteUrl from "next-absolute-url";
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
+export const getStaticProps: GetStaticProps = async (context) => {
   try {
-    const { origin } = absoluteUrl(context.req);
-    const apiURL = `${origin}/api/code`;
-    const res = await fetch(apiURL);
-    const data = await res.json();
-    return { props: { data } };
+    const slug = "code";
+    const params: Params = {
+      version: "published",
+    };
+
+    if (context.preview) {
+      params.version = "draft";
+      params.cv = Date.now();
+    }
+
+    const { data } = await Storyblok.get(
+      `cdn/stories?starts_with=${slug}`,
+      params
+    );
+
+    return {
+      props: {
+        stories: data ? data.stories : false,
+        preview: context.preview || false,
+      },
+      revalidate: 10,
+    };
   } catch (error) {
-    console.log("Error", error);
     return {
       notFound: true,
-      redirect: {
-        destination: "/404",
-        permanent: false,
-      },
-      props: {},
     };
   }
 };
 
-export default function CodePage({ data }: any): JSX.Element {
+export default function CodePage(props): JSX.Element {
+  const stories = useStoryblok(props.stories);
   return (
     <Container p={[2, 3]}>
       <Heading>Code Corner 💻</Heading>
@@ -34,43 +46,44 @@ export default function CodePage({ data }: any): JSX.Element {
       </Subheading>
 
       <Grid sx={{ gridTemplateColumns: ["1fr"], gridAutoRows: "1fr" }}>
-        {data.map((post) => {
-          return (
-            <Card>
-              <Divider />
-              <Flex py={3} sx={{ flexFlow: ["column nowrap", "row nowrap"] }}>
-                <Flex
-                  sx={{
-                    flexFlow: ["column nowrap"],
-                    justifyContent: "center",
-                  }}
-                >
-                  <Text as="h3" color="secondary" mb={2} sx={{ fontSize: 3 }}>
-                    {post.name}
-                  </Text>
-                  <Text as="p" py={1} pr={5}>
-                    {post.content.preview}
-                  </Text>
-                  <Link href={post.full_slug} passHref>
-                    <Text
-                      as="a"
-                      color="grey"
-                      my={2}
-                      sx={{
-                        width: "fit-content",
-                        textDecoration: "none",
-                        "&:hover": { textDecoration: "underline" },
-                      }}
-                    >
-                      Show Me⏵
+        {stories &&
+          stories.map((post) => {
+            return (
+              <Card>
+                <Divider />
+                <Flex py={3} sx={{ flexFlow: ["column nowrap", "row nowrap"] }}>
+                  <Flex
+                    sx={{
+                      flexFlow: ["column nowrap"],
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Text as="h3" color="secondary" mb={2} sx={{ fontSize: 3 }}>
+                      {post.name}
                     </Text>
-                  </Link>
+                    <Text as="p" py={1} pr={5}>
+                      {post.content.preview}
+                    </Text>
+                    <Link href={post.full_slug} passHref>
+                      <Text
+                        as="a"
+                        color="grey"
+                        my={2}
+                        sx={{
+                          width: "fit-content",
+                          textDecoration: "none",
+                          "&:hover": { textDecoration: "underline" },
+                        }}
+                      >
+                        Show Me⏵
+                      </Text>
+                    </Link>
+                  </Flex>
                 </Flex>
-              </Flex>
-              <Divider />
-            </Card>
-          );
-        })}
+                <Divider />
+              </Card>
+            );
+          })}
       </Grid>
     </Container>
   );
