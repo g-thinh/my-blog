a77fa72c3a6a0ecc7a9bef426692849ab0777abe
Thinh Nguyen <gthinh.nguyen@gmail.com>
2021-05-04 22:23:30 -0400

    added storyblok client and useStoryblok hook

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   env: {
     sbKey: process.env.NEXT_PUBLIC_API_TOKEN,
+    sbPreviewKey: process.env.NEXT_PUBLIC_API_PREVIEW,
   },
   images: {
     domains: ["localhost", "a.storyblok.com"],
