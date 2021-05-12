1b5db03ff44cec3e21fcfe0f8535db4684138b8a
Thinh Nguyen <gthinh.nguyen@gmail.com>
2021-05-11 21:15:50 -0400

    fixed internal link, added a tooltip to hidden clickable link

--- components/InternalLink.tsx
+++ components/InternalLink.tsx
@@ -1,10 +1,16 @@
 import { Flex, Text, IconButton } from "theme-ui";
 import { FiLink } from "react-icons/fi";
 import { useEffect } from "react";
+import Tippy from "@tippyjs/react";
+import "tippy.js/dist/tippy.css";
 
 export const InternalLink = (props): JSX.Element => {
   const { text, id } = props.data;
   let pathToCopy = "";
+
+  function handleClick() {
+    navigator.clipboard.writeText(pathToCopy);
+  }
 
   useEffect(() => {
     pathToCopy = window.location.href + "#" + id.url;
@@ -12,7 +18,7 @@ export const InternalLink = (props): JSX.Element => {
 
   return (
     <Flex
-      py={3}
+      my={3}
       sx={{
         alignItems: "center",
         "&:hover button": {
@@ -20,22 +26,24 @@ export const InternalLink = (props): JSX.Element => {
         },
       }}
     >
-      <Text id={id.url} as="h3" color="primary" sx={{ fontSize: [2, 3] }}>
+      <Text id={id.url} color="primary" sx={{ fontSize: [2, 3] }}>
         {text}
       </Text>
-      <IconButton
-        onClick={() => navigator.clipboard.writeText(pathToCopy)}
-        color="lightgrey"
-        sx={{
-          cursor: "pointer",
-          visibility: "hidden",
-          "&:hover": {
-            color: "grey",
-          },
-        }}
-      >
-        <FiLink size={18} />
-      </IconButton>
+      <Tippy content="Copied" placement="right" trigger="click">
+        <IconButton
+          onClick={handleClick}
+          color="grey"
+          sx={{
+            cursor: "pointer",
+            visibility: "hidden",
+            "&:hover": {
+              color: "lightgrey",
+            },
+          }}
+        >
+          <FiLink size={18} />
+        </IconButton>
+      </Tippy>
     </Flex>
   );
 };
