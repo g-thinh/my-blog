2f9c336f7399c4e9079d8e29ad43d3ea84c99710
Thinh Nguyen <gthinh.nguyen@gmail.com>
2021-07-19 20:19:58 -0400

    completed yarn interactive upgrades to package.json

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,16 +1,3 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-
-interface StoryPage {
-  story: Story;
-}
-
-interface StoriesPage {
-  stories: Stories;
-  page?: Story;
-}
-
-interface Params {
-  version: string;
-  cv?: number;
-}
+/// <reference types="next/image-types/global" />
